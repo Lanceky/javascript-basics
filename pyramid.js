de013f55cdcc34e8b50721091c2b290411b6a2cd
Vance_100 <evanslance100@gmail.com>
2024-11-10 13:36:36 +0300

    Update pyramid.js

--- pyramid.js
+++ pyramid.js
@@ -1,6 +1,6 @@
 function gen_pyramid(num_of_rows){
-  for(i= 1, i <= num_of_rows){ 
-    spaces = ' '.repeat(num_of_rows - 1);
+  for(i= 1; i <= num_of_rows; i++){ 
+    spaces = ' '.repeat(num_of_rows - i);
     stars = '*'.repeat(2 * i - 1);
     console.log(spaces + stars + spaces);
 }}
